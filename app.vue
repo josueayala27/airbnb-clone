6194845acb8ae5f3799cebbd9eb45336bd5425ba
Josué Ayala <ayalavalencia11@gmail.com>
2022-12-09 23:35:23 -0600

    🇸🇻 Add footer

--- app.vue
+++ app.vue
@@ -2,4 +2,10 @@
   <div class="flex flex-col gap-5">
     <NuxtPage />
   </div>
+
+  <footer class="fixed bottom-0 left-0 w-full py-[14px] border-t bg-white text-sm">
+    <div class="container px-[24px] mx-auto">
+      © 2022 Not Real Airbnb Site
+    </div>
+  </footer>
 </template>
