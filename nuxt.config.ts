204b13a03364d0181ea0225da5001cd504471c4d
Josué Ayala <ayalavalencia11@gmail.com>
2022-12-17 16:10:18 -0600

    😩 Update `nuxt.config.ts`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
 import svgLoader from 'vite-svg-loader'
 
 export default defineNuxtConfig({
-  nitro: {
-    prerender: {
-      routes: ['/api/hello']
-    }
-  },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge'],
   vite: {
     plugins: [svgLoader()]
