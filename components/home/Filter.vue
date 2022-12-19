fab13d0907839d40371731b073dd26054e017fa4
Josué Ayala <ayalavalencia11@gmail.com>
2022-12-19 00:04:29 -0600

    🌈 Update translations

--- components/home/Filter.vue
+++ components/home/Filter.vue
@@ -3,29 +3,29 @@
     <div class="container mx-auto flex gap-6">
       <div class="relative flex-1 overflow-hidden">
         <div ref="scrollContent" class="scrollbar-hide overflow-auto lg:overflow-hidden flex gap-8 items-center">
-          <HomeFilterItem id="filter-item" selected class="ml-6 lg:ml-0" title="Piscinas increíbles" image="https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg" />
-          <HomeFilterItem id="filter-item" title="Cabañas" image="https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg" />
-          <HomeFilterItem id="filter-item" title="Casas rurales" image="https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg" />
-          <HomeFilterItem id="filter-item" title="Tropical" image="https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg" />
-          <HomeFilterItem id="filter-item" title="Diversión" image="https://a0.muscache.com/pictures/f0c5ca0f-5aa0-4fe5-b38d-654264bacddf.jpg" />
-          <HomeFilterItem id="filter-item" title="Casas compartidas" image="https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg" />
-          <HomeFilterItem id="filter-item" title="Frente a la playa" image="https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg" />
-          <HomeFilterItem id="filter-item" title="Vistas increíbles" image="https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg" />
-          <HomeFilterItem id="filter-item" title="Frente al lago" image="https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg" />
-          <HomeFilterItem id="filter-item" title="Minicasas" image="https://a0.muscache.com/pictures/35919456-df89-4024-ad50-5fcb7a472df9.jpg" />
-          <HomeFilterItem id="filter-item" title="Populares" image="https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg" />
-          <HomeFilterItem id="filter-item" title="Impresionantes" image="https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg" />
-          <HomeFilterItem id="filter-item" title="Casas de árbol" image="https://a0.muscache.com/pictures/4d4a4eba-c7e4-43eb-9ce2-95e1d200d10e.jpg" />
-          <HomeFilterItem id="filter-item" title="Cúpulas" image="https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg" />
-          <HomeFilterItem id="filter-item" title="Castillos" image="https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg" />
-          <HomeFilterItem id="filter-item" title="Novedad" image="https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg" />
-          <HomeFilterItem id="filter-item" title="Parques nacionales" image="https://a0.muscache.com/pictures/c0a24c04-ce1f-490c-833f-987613930eca.jpg" />
-          <HomeFilterItem id="filter-item" title="Mansiones" image="https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg" />
-          <HomeFilterItem id="filter-item" title="Ciudades famosas" image="https://a0.muscache.com/pictures/ed8b9e47-609b-44c2-9768-33e6a22eccb2.jpg" />
-          <HomeFilterItem id="filter-item" title="Aislado" image="https://a0.muscache.com/pictures/9a2ca4df-ee90-4063-b15d-0de7e4ce210a.jpg" />
-          <HomeFilterItem id="filter-item" title="Surf" image="https://a0.muscache.com/pictures/957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg" />
-          <HomeFilterItem id="filter-item" title="Viñedos" image="https://a0.muscache.com/pictures/60ff02ae-d4a2-4d18-a120-0dd274a95925.jpg" />
-          <HomeFilterItem id="filter-item" class="mr-6 lg:mr-0" title="Bed and breakfast" image="https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg" />
+          <HomeFilterItem id="filter-item" selected class="ml-6 lg:ml-0" :title="$t('filters.amazingPools')" image="https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.cabins')" image="https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.ruralHouses')" image="https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.tropical')" image="https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.fun')" image="https://a0.muscache.com/pictures/f0c5ca0f-5aa0-4fe5-b38d-654264bacddf.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.sharedHouses')" image="https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.inFrontOfTheBeach')" image="https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.incredibleViews')" image="https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.lakeFront')" image="https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.tinyHouses')" image="https://a0.muscache.com/pictures/35919456-df89-4024-ad50-5fcb7a472df9.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.popular')" image="https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.impressive')" image="https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.treeHouses')" image="https://a0.muscache.com/pictures/4d4a4eba-c7e4-43eb-9ce2-95e1d200d10e.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.domes')" image="https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.castles')" image="https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.novelty')" image="https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.nationalParks')" image="https://a0.muscache.com/pictures/c0a24c04-ce1f-490c-833f-987613930eca.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.mansions')" image="https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.famousCities')" image="https://a0.muscache.com/pictures/ed8b9e47-609b-44c2-9768-33e6a22eccb2.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.isolated')" image="https://a0.muscache.com/pictures/9a2ca4df-ee90-4063-b15d-0de7e4ce210a.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.surf')" image="https://a0.muscache.com/pictures/957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg" />
+          <HomeFilterItem id="filter-item" :title="$t('filters.vineyards')" image="https://a0.muscache.com/pictures/60ff02ae-d4a2-4d18-a120-0dd274a95925.jpg" />
+          <HomeFilterItem id="filter-item" class="mr-6 lg:mr-0" :title="$t('filters.bedAndBreakfast')" image="https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg" />
         </div>
 
         <!-- Left action button -->
